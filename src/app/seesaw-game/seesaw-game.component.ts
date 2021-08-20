07f7bfe56bfa121a2a902207dc799dfc2fb80215
nojjana <noemi.schuette@students.fhnw.ch>
2021-08-20 23:39:20 +0200

    delete unused code

--- src/app/seesaw-game/seesaw-game.component.ts
+++ src/app/seesaw-game/seesaw-game.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, OnDestroy, ɵɵNgOnChangesFeature } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
-import { SocketService } from '../socket-service/socket.service';
-import { Gyroscope, GyroscopeOptions, GyroscopeOrientation } from '@ionic-native/gyroscope/ngx';
-import { DeviceOrientation, DeviceOrientationCompassHeading, DeviceOrientationCompassOptions } from '@ionic-native/device-orientation/ngx';
-import { environment } from 'src/environments/environment';
-import { Platform } from '@ionic/angular';
+import { DeviceOrientation } from '@ionic-native/device-orientation/ngx';
+import { Gyroscope } from '@ionic-native/gyroscope/ngx';
 import { Vibration } from '@ionic-native/vibration/ngx';
+import { Platform } from '@ionic/angular';
+import { SocketService } from '../socket-service/socket.service';
 
 @Component({
   selector: 'app-seesaw-game',
@@ -20,7 +19,6 @@ export class SeesawGameComponent implements OnInit, OnDestroy {
   public countdown = false;
   public catchingController = true;
   public showMainMenuButton: boolean = false;
-  private devValY: number = 0;
   public devControls;
   public dots = 0;
   private dotInterval;
@@ -33,7 +31,6 @@ export class SeesawGameComponent implements OnInit, OnDestroy {
   currentOY = 0;
   currentOZ = 0;
   justSendedData: any;
-  private devVal: number = 0;
   initHeadingOfController: number;
   private seesawAngle: any;
 
@@ -54,20 +51,11 @@ export class SeesawGameComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.socketService.once('controllerResponsibility', (data) => {
       this.catchingController = data.tutorial;
-      console.log("Catch controller: "+this.catchingController+" / DATA: "+data.tutorial+" / NUMBER: "+data.controllerId);
-
       this.tutorial = data.tutorial;
-
-      // TODO xxx wieder löschen - tutorial wird gleich beendet fürs testing
-      //this.endTutorial();
-
       this.controllerNumber = data.controllerId;
-      console.log("controller number: "+data.controllerId);
-
     });
 
     this.socketService.once('stopSendingData', (mainControllerId) => {
-      console.log('stop sending data');
       this.playing = false;
       this.played = true;
       this.showMainMenuButton = mainControllerId;
@@ -78,13 +66,11 @@ export class SeesawGameComponent implements OnInit, OnDestroy {
 
       clearInterval(this.sensorInterval);
     });
-
     
     this.socketService.on('vibrate', (nrArray) => {
       let controllerId = nrArray[0];
       if (controllerId == this.controllerNumber) {
         this.vibration.vibrate(100);
-        console.log("controller vibrate (sendID, setID): ", controllerId, this.controllerNumber)
       }
     });
     
@@ -92,11 +78,9 @@ export class SeesawGameComponent implements OnInit, OnDestroy {
 
     this.socketService.once('startSendingData', () => {
       this.playing = true;
-      console.log('start Sending data');
+      console.log('start sending data');
 
-      if (this.devControls) {
-        // this.startDevControls();
-      } else {
+      if (!this.devControls) {
         this.startSensor();
       }
     });
@@ -108,49 +92,24 @@ export class SeesawGameComponent implements OnInit, OnDestroy {
     this.socketService.removeListener('controllerResponsibility');
     this.socketService.removeListener('stopSendingData');
     this.socketService.removeListener('startSendingData');
-    this.socketService.removeListener('vibrate');  //TODO add also in other games
+    this.socketService.removeListener('vibrate');
     clearInterval(this.dotInterval);
   }
 
 
 
     /* -------------------- DEV CONTROLS --------------------*/
-
-    private startDevControls(): void {
-  //   this.sensorInterval = setInterval(() => {
-  //     console.log("devVal:", this.devVal);
-  //     this.socketService.emit('controllerData', [this.calculateGravity(this.devVal), this.controllerNumber]);
-  //  }, 1000 / 50)
-  }
   
   public left(): void {
-      // this.devVal += 5;
-
-      // TODO: interval, for better dev control. 
-      //when button click and hold -> keep moving
-      // use (mouseup)=left(false) to clear intervall.. or similar
-
-      // if (doMove){
-      //   this.interv = setInterval(() => {
-      //     this.name = "has been long pressed"
-      //     this.interv = null;
-      //   }, 500);
-
-        this.socketService.emit('controllerData', [-0.4, this.controllerNumber]);
-      // } else {
-
-      // }
-
+    this.socketService.emit('controllerData', [-0.6, this.controllerNumber]);
   }
 
   public center(): void {
-    // this.devVal -= 5;
     this.socketService.emit('controllerData', [0, this.controllerNumber]);
 }
 
   public right(): void {
-      // this.devVal -= 5;
-      this.socketService.emit('controllerData', [0.4, this.controllerNumber]);
+    this.socketService.emit('controllerData', [0.6, this.controllerNumber]);
   }
 
   /* -------------------- SENSOR METHODS --------------------*/
@@ -158,175 +117,10 @@ export class SeesawGameComponent implements OnInit, OnDestroy {
     
   private startSensor() {
     this.startAccelerometerSensor();
-  //  this.startGyroOrientationSensor();
-    // this.startDeviceOrientationSensor();
   }
 
-  // private startDeviceOrientationSensor() {
-  //   // while (this.initHeadingOfController == undefined) {
-  //   //   console.log("Getting initial magneticHeading heading of controller...", this.initHeadingOfController);
-  //   //   this.determineInitHeadingOfController();
-  //   // }
-  //   // console.log("initHeadingOfController:", this.initHeadingOfController);
-  //   this.sensorInterval = setInterval(() => {
-  //   // Get the device current compass heading
-  //   this.deviceOrientation.getCurrentHeading()
-  //   .then(
-  //     (data: DeviceOrientationCompassHeading) => this.processDeviceOrientationData(data),
-  //     (error: any) => console.log(error)
-  //   );
-  //   }, 1000 / 50);
-
-    // Watch the device compass heading change
-// var subscription = this.deviceOrientation.watchHeading().subscribe(
-//   (data: DeviceOrientationCompassHeading) => console.log(data)
-// );
-
-  // }
-
-  // private processDeviceOrientationData(data: DeviceOrientationCompassHeading): any {
-  //   if (this.initHeadingOfController == undefined || this.initHeadingOfController <= 0) {
-  //     this.determineInitHeadingOfController(data);
-  //   } else {
-  //     let currentHeading = data.magneticHeading;
-  //     if (currentHeading != null && currentHeading >= 1) {
-  //       console.log("magneticHeading:", currentHeading);
-  //       this.socketService.emit('controllerData', [this.calculateOrientation(currentHeading), this.controllerNumber]);
-  //     }
-  //   }
-
-  // }
-
-  // private determineInitHeadingOfController(data: DeviceOrientationCompassHeading) {
-  //   // startposition of phone (reference for center)
-  //   // -> after countdown: hold it pointing to the center of the screen!
-  //   this.initHeadingOfController = data.magneticHeading;
-  //   console.log("Getting initial magneticHeading of controller...", this.initHeadingOfController);
-  // }
-
-  // private calculateOrientation(currentHeading: number): any {
-
-  //   let val = null;
-  //   let threshold = 20;
-
-  //   // norm currentHeading as if initHeadingOfController was 0
-  //   // if > 0 && < 180 --> right, if < 360 &&  > 180 --> left
-  //   let currentHeadingNormed = currentHeading - this.initHeadingOfController;
-  //   if (currentHeadingNormed > 360) {
-  //     currentHeadingNormed = 360 - currentHeadingNormed;
-  //   } else if (currentHeadingNormed < 0) {
-  //     currentHeadingNormed = currentHeadingNormed + 360;
-  //   }
-    
-  //   // left or right?
-  //   if (currentHeadingNormed > threshold && currentHeadingNormed < (90+threshold)) {
-  //     // right
-  //     val = 1;
-  //   } else if (currentHeadingNormed < (360-threshold) && currentHeadingNormed > (270-threshold)) {
-  //     // left
-  //     val = -1;
-  //   } else if (currentHeadingNormed < threshold || currentHeadingNormed > (360-threshold)) {
-  //     // center
-  //     val = 0;
-  //   } else {
-  //     // do nothing
-  //     val = null;
-  //   }
-
-
-    // // initial calculation version: beachted sprung 360 zu 0 nicht...
-    // let diff = currentHeading - this.initHeadingOfController;
-    // if (diff > threshold) {
-    //   // right
-    //   val = 1;
-    // } else if (diff < -threshold) {
-    //   // left
-    //   val = -1;
-    // } else {
-    //   // center
-    //   val = 0;
-    // }
-
-
-    // // fix, other calculation version: calculate opposite degree, check if on right or left side
-    // let oppositeDegree = this.initHeadingOfController + 180;
-    // if (this.initHeadingOfController < oppositeDegree) {
-    //   // left: change 360 to 0
-    //   // right: normally increasing
-    //   if (this.initHeadingOfController < currentHeading && currentHeading < oppositeDegree) {
-    //     // moved to the right
-    //     val = 1;
-    //   } else {
-    //     val = -1;
-    //   }
-    // } else {
-    //   // left: normally decreasing
-    //   // right: change 360 to 0
-    //   if (this.initHeadingOfController > currentHeading && currentHeading > oppositeDegree) {
-    //     // moved to the left
-    //     val = -1;
-    //   } else {
-    //     val = 1;
-    //   }
-    // }
-
-  //   return val;
-  // }
-
-
-  // gyro: rotation/twist of phone
-  // private startGyroOrientationSensor(): void {
-  //   let options: GyroscopeOptions = {
-  //     frequency: 100
-  //   }
-
-  //   this.sensorInterval = setInterval(() => {
-  //     this.gyroscope.getCurrent(options)
-  //       .then(
-  //         (orientation: GyroscopeOrientation) => {
-  //           this.processGyroOrientationData(orientation);
-  //         })
-  //       .catch()
-  //   }, 1000 / 50);
-  // }
-
-  // private processGyroOrientationData(orientation: GyroscopeOrientation) {
-  //   let val = orientation.x;
-
-  //   if (orientation.x > 2 || orientation.x < -2) {
-
-  //  // if (val != null && val != 0) {
-  //     this.socketService.emit('controllerData', [orientation.x, this.controllerNumber]);
-  //     //this.socketService.emit('controllerData', [this.calculateGravity(val), this.controllerNumber]);
-  //     console.log("orientation", orientation.x, orientation.y, orientation.z);
-  //   }
-  //   // this.socketService.emit('controllerData', [orientation.y]);
-  //   // console.log("orientation z", orientation.z);
-  //   // this.socketService.emit('controllerData', [orientation.z]);
-  // }
-
-
-  
-  // private calculateGravity(val: number): number {
-  //   let threshold = 20;
-
-  //   threshold = 10;
-  //   val = -val;
-  
-  //   if(val > threshold){
-  //     val = 1;
-  //   } else if(val < -threshold){
-  //     val = -1;
-  //   } else {
-  //     val = 1 / threshold * val;
-  //   }
-
-  //   return val;
-  // }
-
-
-  // accelerometer: beschleunigung in 3 achsen
-  // doku von ionic: https://ionicframework.com/docs/native/device-motion
+  // accelerometer in x y z axis
+  // docs: https://ionicframework.com/docs/native/device-motion
   private startAccelerometerSensor(): void {
     this.sensorInterval = setInterval(() => {
       this.deviceMotion.getCurrentAcceleration()
@@ -337,56 +131,23 @@ export class SeesawGameComponent implements OnInit, OnDestroy {
     }, 1000 / 60);   
   }
 
-  //round acceleration to full value to avoid .xx values
-  private processAccelData(acceleration: DeviceMotionAccelerationData) {
-    console.log("acceleration.y: "+acceleration.y);
-  //  let seesawAngle;
-
-  //  let accelerationY = Math.floor(acceleration.y * 10000)/10000;
-
-  //  let calculatedAngle = this.calculateAngle(accelerationY);
-    this.calculateAngle(acceleration.y)
-  
-    //TODO: SWITCH CASE
-    // rechts -> > 1 
+  private processAccelData(acceleration: DeviceMotionAccelerationData) { 
+    // right -> > 1 
     if (acceleration.y >= 3 && this.seesawAngle != 0.6){
-      this.seesawAngle = 0.6 // 4.0 
+      this.seesawAngle = 0.6 
       this.socketService.emit('controllerData', [this.seesawAngle, this.controllerNumber]);
-      console.log("Sended seesawAngle (plus):" + this.seesawAngle);
     } 
-    // links -> >-1
-    else if (acceleration.y <= -3 && this.seesawAngle != -0.5){
-      this.seesawAngle = -0.6 //-4.0 
+    // left -> > -1
+    else if (acceleration.y <= -3 && this.seesawAngle != -0.6){
+      this.seesawAngle = -0.6
       this.socketService.emit('controllerData', [this.seesawAngle, this.controllerNumber]);
-      console.log("Sended seesawAngle (minus):" + this.seesawAngle);
     } 
     // center when between 1 and -1
     else if (acceleration.y < 3 && acceleration.y > -3 && this.seesawAngle != 0){
       this.seesawAngle = 0 
       this.socketService.emit('controllerData', [this.seesawAngle, this.controllerNumber]);
-      console.log("Sended seesawAngle (zero)): " + this.seesawAngle);
     }
-
-    /* if (acceleration.y < 5 && acceleration.y > -5) {
-      //  if (!this.justSendedData) {
-          console.log("acceleration Y", acceleration.y);
-          
-          this.socketService.emit('controllerData', [acceleration.y, this.controllerNumber]);
-        //  this.vibration.vibrate(100);
-          this.justSendedData = true;
-
-/*           setTimeout(() => {
-            this.justSendedData = false;
-          }, 500); //500 */
-        //}
-     // } 
   }
-
-  private calculateAngle(accelerationY){
-    let val = accelerationY*-9;
-    //console.log("Calculation of Angle: "+val);
-  }
-
 
     /* -------------------- BASIC GAME METHODS --------------------*/
 
@@ -400,8 +161,6 @@ export class SeesawGameComponent implements OnInit, OnDestroy {
     }
   
     public quitGame(): void {
-      //console.log("ionic: quitGame() called");
-      //this.controllerQuitGame = true;
       this.socketService.emit('quitGame');
     }
 
